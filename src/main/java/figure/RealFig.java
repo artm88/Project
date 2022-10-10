4000078d28592b05fc0e0ebec42d9a846375422c
artm88 <akoryukin@gmail.com>
2022-10-10 17:23:14 +0300

    change readme

--- src/main/java/figure/RealFig.java
+++ src/main/java/figure/RealFig.java
@@ -3,6 +3,6 @@ package figure;
 public class RealFig {
     public static void main(String[] args) {
         figRect fig=new figRect("красный"); // создание фигуры
-        fig.draw(5,5,"красный"); // изображение фигуры
+        fig.draw(10,1,"красный"); // изображение фигуры
     }
 }
